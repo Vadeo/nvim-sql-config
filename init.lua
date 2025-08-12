193dbfd186f1bf75db519d046d796289fd111afb
Vadim Selin <vadim-selin-sbt@yandex.ru>
2025-08-12 21:47:50 +0300

    Toggle off vim-visual-multi

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.options")
 require("config.keymaps")
 require("plugins.lazy")
-require("config.visual_multi")
+--require("config.visual_multi")
 --vim.cmd("colorscheme tokyonight")
 -- Цветовая схема
 vim.cmd("colorscheme nordfox")
