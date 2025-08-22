2e8d247140b32cf762e9a16dc08e2e005b92f6ce
Vadim Selin <vadim-selin-sbt@yandex.ru>
2025-08-22 22:29:28 +0300

    Nordfox colorscheme plugged

--- init.lua
+++ init.lua
@@ -3,8 +3,8 @@ require("config.keymaps")
 require("plugins.lazy")
 require("config.visual_multi")
 -- Цветовая схема
---vim.cmd("colorscheme nightfox")
-vim.g.everforest_background = "medium" -- "soft", "medium" или "hard"
-vim.g.everforest_enable_italic = 1
-vim.g.everforest_better_performance = 1
-vim.cmd("colorscheme everforest")
+vim.cmd("colorscheme nordfox")
+--vim.g.everforest_background = "medium" -- "soft", "medium" или "hard"
+--vim.g.everforest_enable_italic = 1
+--vim.g.everforest_better_performance = 1
+--vim.cmd("colorscheme everforest")
